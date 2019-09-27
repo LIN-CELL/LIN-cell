f64457d09de2f9e12ec40b8df660f19b514943a3
lin <1751931646@qq.com>
2019-09-27 14:36:46 +0800

    书本70页程序

---
+++ ForArrage.java
@@ -0,0 +1,25 @@
+public class ForArrage{
+	/*public static void main(String[] args){
+		
+		int[] arrage = new int[(int)(Math.random()*10)+1];
+
+        for(int i=0;i<arrage.length;i++){
+			arrage[i] = (int)(Math.random()*100);
+		}		
+		for( int value:arrage){
+			System.out.println(value);
+		}
+	}*/
+	public static void main(String[] args){
+		
+		int[] arrage = new int[(int)(Math.random()*10)+1];
+
+        for(int i=0;i<arrage.length;i++){
+			
+			arrage[i] = (int)(Math.random()*100);
+		
+			System.out.println(arrage[i]);
+		
+	}
+	}
+}
