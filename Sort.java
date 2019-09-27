7b5388b3722245ed6e8c7c95643c987856251a8a
lin <1751931646@qq.com>
2019-09-27 23:47:20 +0800

    Sort()的用法

---
+++ Sort.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Sort {
+    public static void main(String[] args){
+
+        int[] a = {9, 8, 7, 2, 3, 4, 1, 0, 6, 5};
+
+        Arrays.sort(a);
+
+        for(int i = 0; i < a.length; i ++) {
+
+                         System.out.print(a[i] + " ");
+                 }
+
+    }
